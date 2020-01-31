cc4776a60a08519d8e144eacc573a9ce95f4b622
dannyseymour <daniel@discordia.io>
2020-01-31 14:11:58 -0700

    added mappers and restdocs

---
+++ src/test/java/io/discordia/msscbrewery/web/controller/BeerControllerTest.java
@@ -0,0 +1,53 @@
+package io.discordia.msscbrewery.web.controller;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+@ComponentScan(basePackages = "io.discordia.msscbeerservice.web.mappers")
+
+@AutoConfigureRestDocs
+@ExtendWith(RestDocumentationExtension.class)
+@WebMvcTest(BeerController.class)
+
+class BeerControllerTest {
+
+    @Test
+    void getBeer() {
+    }
+
+    @Test
+    void handlePost() {
+    }
+
+    @Test
+    void handleUpdate() {
+    }
+
+    @Test
+    void handleDelete() {
+    }
+}
